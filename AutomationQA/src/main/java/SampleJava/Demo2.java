04b46f662c9dce281e6a676ea1c1a2cd87a77890
vedpr <sujoymandal33@gmail.com>
2019-12-15 21:47:10 +0530

    commit

--- AutomationQA/src/main/java/SampleJava/Demo2.java
+++ AutomationQA/src/main/java/SampleJava/Demo2.java
@@ -4,11 +4,7 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 
-		System.out.println("updated in feature1");
-		System.out.println("added in feature2!!");
-		System.out.println("added again in feature1!!");
-		System.out.println("updated in feature2!!");
-		System.out.println("updated in feature1!!");
+		System.out.println("fresh update in feature1");
 	}
 
 }
