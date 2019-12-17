b8bfb8ff4019bdeff9f58aef066c62a074dbf65d
vedpr <sujoymandal33@gmail.com>
2019-12-17 10:06:19 +0530

    commit

--- AutomationQA/src/main/java/SampleJava/Demo2.java
+++ AutomationQA/src/main/java/SampleJava/Demo2.java
@@ -5,12 +5,12 @@ public class Demo2 {
 	public static void main(String[] args) {
 
 		System.out.println("fresh update in feature1");
-		//updating in feature2
+		//no need to have this
 		//updating in feature3
-		//updates in feature3
+		//updating in feature1
 		//needs to take the updates from feature1
 		//updating in feature1
-		//updates in feature1
+		//duplicate
 
 
 	}
